d15b4e3d9e669935730eb417ebbc52802aeffd1f
Joseph <Ethan.Joseph@tdameritrade.com>
2017-08-25 14:44:32 -0400

    EJOS added a collection view to display the tag buttons

--- IdeaTracker/EditorViewController.swift
+++ IdeaTracker/EditorViewController.swift
@@ -4,6 +4,8 @@ class EditorViewController: UIViewController {
 
     @IBOutlet weak var notesTextView: UITextView!
     
+    fileprivate let reuseIdentifier = "TagCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         styleNotesView()
@@ -16,5 +18,23 @@ class EditorViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+}
+
+extension EditorViewController: UICollectionViewDataSource{
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier,
+                                                      for: indexPath)
+        cell.backgroundColor = UIColor.black
+        // Configure the cell
+        return cell
     }
 }
