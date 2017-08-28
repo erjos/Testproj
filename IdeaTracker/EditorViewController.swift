440dfe5c69dbe6622df38e08eecc50e195b5947b
Joseph <Ethan.Joseph@tdameritrade.com>
2017-08-28 16:03:56 -0400

    EJOS tested adding an add button into the tag collection view to give users an option to add more tags

--- IdeaTracker/EditorViewController.swift
+++ IdeaTracker/EditorViewController.swift
@@ -1,15 +1,12 @@
 import UIKit
 
 class EditorViewController: UIViewController {
-
-    @IBOutlet weak var backButton: UIButton!
-    @IBOutlet weak var savebutton: UIButton!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var notesTextView: UITextView!
     
     fileprivate let reuseIdentifier = "TagCell"
     
-    let defaultTags = ["Book", "Movie", "Quote", "Idea", "Technology", "Product", "Marketing", "Work", "Random", "Fun"]
+    let defaultTags = ["+", "Book", "Movie", "Quote", "Idea", "Technology", "Product", "Marketing", "Work", "Random", "Fun"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,8 +18,6 @@ class EditorViewController: UIViewController {
         
         //TODO: fix round corners so it can take any number of views from 1 ...
         notesTextView.roundCorners()
-        backButton.roundCorners()
-        savebutton.roundCorners()
     }
     
     override func didReceiveMemoryWarning() {
@@ -40,9 +35,10 @@ extension EditorViewController: UICollectionViewDataSource{
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier,
                                                       for: indexPath) as! TagCollectionViewCell
-        
         let title = defaultTags[indexPath.row]
         cell.button.setTitle(title, for: .normal)
         cell.backgroundColor = UIColor.clear
@@ -56,14 +52,14 @@ extension EditorViewController: UICollectionViewDataSource{
 extension EditorViewController: UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        var standardTagWidth: CGFloat = 60
+        var standardTagWidth: CGFloat = 30
         let standardTagHeight: CGFloat = 30
         
         let tagLength = CGFloat(defaultTags[indexPath.row].characters.count)
-        if(tagLength > 5){
-            let diff:CGFloat = tagLength - 5
+        if(tagLength > 3){
+            let diff:CGFloat = tagLength - 3
             let space:CGFloat = diff * 10
-            standardTagWidth = 60 + space
+            standardTagWidth = 30 + space
         }
         //let itemsPerRow:CGFloat = 4
         //intended to Calculates all the padding that the row will contain
