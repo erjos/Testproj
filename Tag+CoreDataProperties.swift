b4316a2850c495d6570c4ea2bddd3f1432960714
Joseph <Ethan.Joseph@tdameritrade.com>
2017-09-01 12:45:40 -0400

    created some object models of the entities in core data

---
+++ Tag+CoreDataProperties.swift
@@ -0,0 +1,31 @@
+import Foundation
+import CoreData
+
+
+extension Tag {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Tag> {
+        return NSFetchRequest<Tag>(entityName: "Tag");
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var entries: NSSet?
+
+}
+
+// MARK: Generated accessors for entries
+extension Tag {
+
+    @objc(addEntriesObject:)
+    @NSManaged public func addToEntries(_ value: Entry)
+
+    @objc(removeEntriesObject:)
+    @NSManaged public func removeFromEntries(_ value: Entry)
+
+    @objc(addEntries:)
+    @NSManaged public func addToEntries(_ values: NSSet)
+
+    @objc(removeEntries:)
+    @NSManaged public func removeFromEntries(_ values: NSSet)
+
+}
