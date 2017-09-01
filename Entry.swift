823c6923004b57c0193b1e0f0ce167fc5321b09f
Joseph <Ethan.Joseph@tdameritrade.com>
2017-09-01 13:02:38 -0400

    consolidated core data model files

--- Entry.swift
+++ Entry.swift
@@ -5,3 +5,32 @@ import CoreData
 public class Entry: NSManagedObject {
 
 }
+
+extension Entry {
+    
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Entry> {
+        return NSFetchRequest<Entry>(entityName: "Entry");
+    }
+    
+    @NSManaged public var name: String?
+    @NSManaged public var notes: String?
+    @NSManaged public var tags: NSSet?
+    
+}
+
+// MARK: Generated accessors for tags
+extension Entry {
+    
+    @objc(addTagsObject:)
+    @NSManaged public func addToTags(_ value: Tag)
+    
+    @objc(removeTagsObject:)
+    @NSManaged public func removeFromTags(_ value: Tag)
+    
+    @objc(addTags:)
+    @NSManaged public func addToTags(_ values: NSSet)
+    
+    @objc(removeTags:)
+    @NSManaged public func removeFromTags(_ values: NSSet)
+    
+}
--- Tag.swift
+++ Tag.swift
@@ -5,3 +5,31 @@ import CoreData
 public class Tag: NSManagedObject {
 
 }
+
+extension Tag {
+    
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Tag> {
+        return NSFetchRequest<Tag>(entityName: "Tag");
+    }
+    
+    @NSManaged public var name: String?
+    @NSManaged public var entries: NSSet?
+    
+}
+
+// MARK: Generated accessors for entries
+extension Tag {
+    
+    @objc(addEntriesObject:)
+    @NSManaged public func addToEntries(_ value: Entry)
+    
+    @objc(removeEntriesObject:)
+    @NSManaged public func removeFromEntries(_ value: Entry)
+    
+    @objc(addEntries:)
+    @NSManaged public func addToEntries(_ values: NSSet)
+    
+    @objc(removeEntries:)
+    @NSManaged public func removeFromEntries(_ values: NSSet)
+    
+}
