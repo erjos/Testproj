e2d197ae3ba80018bf84132abeae753975d58dfb
Joseph <Ethan.Joseph@tdameritrade.com>
2017-09-06 15:23:12 -0400

    table view displays data

--- IdeaTracker/HomeViewController.swift
+++ IdeaTracker/HomeViewController.swift
@@ -3,23 +3,10 @@ import RealmSwift
 
 class HomeViewController: UIViewController {
     
-    //APP TODOS
-    //- make dividers on table view for main page the same color as the stroke dividers on the editor page
-    
-    //- add info popup to the editor that describes tags: "Think of tags as collections or folders you can use to order and quickly retrieve your saved entries. You can add multiple tags to your entries and create new custom tags. You can view your entries by tag and search for them as well."
-    
-    //- create function to create custom tags
-    
-    //- design and build schema for core data
-    
-    //- Tab bar on main page to display multiple views of data: table view of chronological order, collection view of tags/folders, etc.
-    
-    //- add shadows where applicable to make the UI pop more (i.e. around the tags that are selected to portray a pressed button to the user
-    
-    //- youtube style tab bar under the nav bar on the main page - white selector bar with dropshadow under it display which tab is selected - title on the left hand side of the nav bar provide a label
-    
     var entries: Results<Entry>?
 
+    @IBOutlet weak var entryTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
@@ -27,11 +14,16 @@ class HomeViewController: UIViewController {
         let logo = UIImage(named: "Pencil")
         imageView.image = logo
         self.navigationItem.titleView = imageView
+        
+        
+        entryTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         let realm = try! Realm()
         entries = realm.objects(Entry.self)
+        entryTableView.reloadData()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -41,14 +33,36 @@ class HomeViewController: UIViewController {
 extension HomeViewController: UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return (entries?.count)!
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        let entry = entries?[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = entry?.name
+        
+        return cell
     }
 }
 
 extension HomeViewController: UITableViewDelegate{
     
 }
+
+//APP TODOS
+//- make dividers on table view for main page the same color as the stroke dividers on the editor page
+
+//- add info popup to the editor that describes tags: "Think of tags as collections or folders you can use to order and quickly retrieve your saved entries. You can add multiple tags to your entries and create new custom tags. You can view your entries by tag and search for them as well."
+
+//- create function to create custom tags
+
+//- design and build schema for core data
+
+//- Tab bar on main page to display multiple views of data: table view of chronological order, collection view of tags/folders, etc.
+
+//- add shadows where applicable to make the UI pop more (i.e. around the tags that are selected to portray a pressed button to the user
+
+//- youtube style tab bar under the nav bar on the main page - white selector bar with dropshadow under it display which tab is selected - title on the left hand side of the nav bar provide a label
