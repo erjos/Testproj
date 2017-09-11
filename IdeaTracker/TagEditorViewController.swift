015868142b8ebddc899485cf00011dbcb9cc2f0d
Joseph <Ethan.Joseph@tdameritrade.com>
2017-09-11 14:38:42 -0400

    present alert modal if the tag already exists in the realm

--- IdeaTracker/TagEditorViewController.swift
+++ IdeaTracker/TagEditorViewController.swift
@@ -21,17 +21,27 @@ class TagEditorViewController: UIViewController {
     @IBAction func saveAction(_ sender: Any) {
         
         if let tag = tagName {
-            //update the tag name in realm
+            //update the tag name in realm - make sure this doesn't screw up other stuff before we add this feature...
         }
         
         if let tagName = tagNameField.text{
             //save the new tag to realm
             let realm = try! Realm()
-            let tag = Tag()
-            tag.name = tagName
             
-            try! realm.write {
-                realm.add(tag)
+            //check if there are any tags with this name present.
+            if(realm.objects(Tag.self).filter("name == %@", tagName).count == 0){
+                let tag = Tag()
+                tag.name = tagName
+                
+                try! realm.write {
+                    realm.add(tag)
+                }
+            } else {
+                //present an alert that the tag name already exists
+                let alertController = UIAlertController(title: "Tag already exists.", message: "choose a different name", preferredStyle: .alert)
+                let defaultAction = UIAlertAction(title: "Try Again", style: .default, handler: nil)
+                alertController.addAction(defaultAction)
+                self.present(alertController, animated: true, completion: nil)
             }
         }
     }
