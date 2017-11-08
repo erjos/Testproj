12d3db0a6916e5d277de242fc3b0deca11164361
Joseph <Ethan.Joseph@tdameritrade.com>
2017-11-08 16:24:06 -0500

    EJOS updated cell color for table and separator defaults to make it more consistent

--- Tag.swift
+++ Tag.swift
@@ -2,6 +2,7 @@ import Foundation
 import RealmSwift
 
 public class Tag: Object {
+    //realm must be written in objc if it requires this annotation?
     @objc dynamic var name = ""
     let entries = LinkingObjects(fromType: Entry.self, property: "tags")
 }
